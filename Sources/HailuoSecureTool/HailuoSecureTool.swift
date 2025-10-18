4f59f6622b2309bf984eea82fe49203a7050037c
yojjoyy1 <yojjoyy1@gmail.com>
2025-10-18 15:27:03 +0800

    放入一個測試程式碼，測試pkg拉下來後能不能用

--- Sources/HailuoSecureTool/HailuoSecureTool.swift
+++ Sources/HailuoSecureTool/HailuoSecureTool.swift
@@ -1,6 +1,12 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-func initTest(){
-    print("測試成功")
+public struct HailuoTestArea{
+    public var version = "1.0.0"
+    public func initTest(){
+        print("測試成功")
+    }
+    public init () {
+        print("\(#function) 初始化成功")
+    }
 }
